52f0345050213451ff86ea63e3f8649a3e7f0e93
Dilshodbek <104253915+Dilshodbek9505@users.noreply.github.com>
2022-04-28 06:28:02 +0500

    Update paytun masalalar yechimi.py

--- paytun masalalar yechimi.py
+++ paytun masalalar yechimi.py
@@ -1,15 +1,3 @@
- k,n=map(int,input('sonlarni kiriting: ').split())
- for i in range(0,n):
-     print(k,end=" ")
-
- for i in range(1,11):
-     for x in range(1,11):
-         print(str(x)+"*"+str(i)+"="+str(i*x),end="   ")
-     print()
-
- print("salom"+" Ali"+str(45))
-
-
 # archani yasash kodi    
  n=int(input('n= '))
  for i in range(n):
@@ -55,32 +43,3 @@
          print(s,end="")
          s+=1
      print()
-
-
-# while operatori ichiga shart berish mumkiun 
- a=1
- while a<=100:
-    print(a)
-    a+=1
-
-
- a=1
- while a<=100:
-    print(a)
-    a+=1
-    if a==96:
-         break
-
- a=0
- while a<=100:
-     a+=1
-     if a==96:
-         continue
-     print(a)
-
- a=0
- while a<=100:
-     a+=1
-     if a%2==1:
-         continue
-     print(a)
